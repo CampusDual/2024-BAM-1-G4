3802bd89ce9cd5d35e0fb8a1ece3b6a2b53b012b
zYoelz <vip.yoel@gmail.com>
2025-01-21 12:16:39 +0100

    add activity_confirm_email.xml add ActivityConfirmEmail update other activitys

---
+++ app/src/main/java/com/app/ecostep/ActivityConfirmEmail.kt
@@ -0,0 +1,14 @@
+package com.app.ecostep
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityConfirmEmail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_email)
+    }
+}
